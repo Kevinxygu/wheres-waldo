655178f1b8920266c4c119e22e3819f81e13016d
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-08-17 21:41:04 -0700

    added page when page on mobile is a bit too small

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/Home/Home'
-import Settings from './components/Settings/Settings'
-import GameScreen from './components/Game/GameScreen'
-import Terms from './components/Terms/Terms'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Home from './components/Home/Home';
+import Settings from './components/Settings/Settings';
+import GameScreen from './components/Game/GameScreen';
+import Terms from './components/Terms/Terms';
 
-// main app screen (conditially renders home, settings, game, or terms)
+// main app screen (conditionally renders home, settings, game, or terms)
 const App = () => {
   // Initial state
   const [currentPage, setCurrentPage] = useState('home');
   const [difficulty, setDifficulty] = useState('easy');
   const [time, setTime] = useState(60);
-  const [goal, setGoal] = useState("waldo");
+  const [goal, setGoal] = useState('waldo');
+  const [isScreenSmall, setIsScreenSmall] = useState(window.innerWidth < 900);
+
+  useEffect(() => {
+    // Function to handle resizing the screen if it's too small (900 px, for the width of the game)
+    const handleResize = () => {
+      setIsScreenSmall(window.innerWidth < 900);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const changeDifficulty = (diff) => {
     setDifficulty(diff);
@@ -32,7 +45,7 @@ const App = () => {
 
   let pageComponent;
   if (currentPage === 'home') {
-    pageComponent = <Home changePage={changePage}/>;
+    pageComponent = <Home changePage={changePage} />;
   } else if (currentPage === 'settings') {
     pageComponent = (
       <Settings
@@ -46,18 +59,30 @@ const App = () => {
       />
     );
   } else if (currentPage === 'game') {
-    pageComponent = <GameScreen changePage={changePage}
-     difficulty={difficulty}
-      time={time}
-      goal={goal} />
+    pageComponent = (
+      <GameScreen
+        changePage={changePage}
+        difficulty={difficulty}
+        time={time}
+        goal={goal}
+      />
+    );
   } else if (currentPage === 'terms') {
     pageComponent = <Terms changePage={changePage} />;
   }
+
   return (
     <div>
-      {pageComponent}
+      {isScreenSmall ? (
+        <div className="smallScreenMessage">
+          <img src="/woof.png" alt="Waldo's dog on the page where screen is a bit too small" className="tooSmallImage"/>
+          <p className="smallScreenText">Your screen is too small! I'm currenty working on bringing Where's Waldo to this screen size :)</p>
+        </div>
+      ) : (
+        pageComponent
+      )}
     </div>
   );
-}
+};
 
-export default App
+export default App;
